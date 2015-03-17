39b54f8e90ce03a10a618591dd17daaf9fcbaf49
Lennard Timm <lennard@71grad.de>
2015-03-17 18:20:05 +0100

    Add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
-alias reload!='. ~/.zshrc'
+alias ll='ls -lh'
+alias la='ls -lah'
+
+alias rld='. ~/.zshrc'
+
+alias j=z
+alias mkcd=take
+
+alias dotfiles='mate ~/.dotfiles'
+alias zshrc='mate ~/.zshrc'
