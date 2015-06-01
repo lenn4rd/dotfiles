879751c53a5aad6f7600ebd3b692f52db4873355
Lennard Timm <lennard@71grad.de>
2015-06-01 13:16:18 +0200

    Add functions

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,7 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then
+  fpath=($topic_folder $fpath)
+fi;
+
+fpath=($HOME/.dotfiles/functions $fpath)
+autoload -U $HOME/.dotfiles/functions/*(:t)
