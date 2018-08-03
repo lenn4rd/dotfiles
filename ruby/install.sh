3c03d71a65d0530c1a2761e0687d630efbdb163c
Lennard Timm <hi@lenn4rd.io>
2018-08-03 18:35:45 +0200

    Add rbenv binstubs plugin

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,5 +4,6 @@ cd $(rbenv root)/plugins
 
 git clone https://github.com/rkh/rbenv-update.git
 git clone https://github.com/sstephenson/rbenv-default-gems.git
+git clone https://github.com/ianheggie/rbenv-binstubs.git
 
 bundle config build.puma --with-opt-dir=/usr/local/opt/openssl
