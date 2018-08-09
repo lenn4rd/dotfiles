730cdf7d5f2ad5d4d279816771cd509375f611b1
Lennard Timm <hi@lenn4rd.io>
2018-08-09 11:26:52 +0200

    Remove cruft

--- ssh/completion.zsh
+++ ssh/completion.zsh
@@ -1,2 +1,2 @@
 hosts=$(awk '/^Host / {print $2}' ~/.ssh/config 2>/dev/null | tr ' ' "\n")
-zstyle ':completion:*:hosts' hosts $hosts #"`cat ~/.ssh/config | grep 'Host ' | cut -f 2 -d ' ' | tr ',' '\n'`"
+zstyle ':completion:*:hosts' hosts $hosts
