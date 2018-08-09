83d9b6278706ba20c2cf9647e316538fdcd8f1c2
Lennard Timm <hi@lenn4rd.io>
2018-08-09 11:22:47 +0200

    Update Ruby configuration

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,8 +2,21 @@ mkdir -p $(rbenv root)/plugins
 
 cd $(rbenv root)/plugins
 
+git clone https://github.com/ianheggie/rbenv-binstubs.git
+git clone https://github.com/rbenv/rbenv-each.git
 git clone https://github.com/rkh/rbenv-update.git
 git clone https://github.com/sstephenson/rbenv-default-gems.git
-git clone https://github.com/ianheggie/rbenv-binstubs.git
 
-bundle config build.puma --with-opt-dir=/usr/local/opt/openssl
+cat > $(rbenv root)/default-gems <<EOF
+awesome_print
+bundler
+byebug
+byebug-color-printer
+foreman
+qwandry
+EOF
+
+RUBY_VERSION=2.5.1
+
+rbenv install $RUBY_VERSION
+rbenv global $RUBY_VERSION
