45b4f1070bb30ce1d745e556d0389165cf0b6009
Lennard Timm <hi@lenn4rd.io>
2019-09-03 15:07:03 +0200

    Set Python, Ruby ENV variables

---
+++ python/env.zsh
@@ -0,0 +1,3 @@
+# Allow running multithreaded Python scripts
+# Source: https://stackoverflow.com/a/52230415
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
---
+++ ruby/env.zsh
@@ -0,0 +1,3 @@
+# Use multiple threads when installing gems
+export BUNDLE_JOBS=4
+export MAKEFLAGS=-j4
