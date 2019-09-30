5f22acdd8958bde01c4b04d4235c10706c95e803
Lennard Timm <hi@lenn4rd.io>
2019-09-30 12:02:55 +0200

    Remove rbenv-binstubs plugin

--- ruby/install.sh
+++ ruby/install.sh
@@ -2,7 +2,6 @@ mkdir -p $(rbenv root)/plugins
 
 cd $(rbenv root)/plugins
 
-test -d rbenv-binstubs     || git clone https://github.com/ianheggie/rbenv-binstubs.git
 test -d rbenv-each         || git clone https://github.com/rbenv/rbenv-each.git
 test -d rbenv-update       || git clone https://github.com/rkh/rbenv-update.git
 test -d rbenv-default-gems || git clone https://github.com/sstephenson/rbenv-default-gems.git
