67910ee2b49e2893c3a17a1b291ae29f3cf91c6b
Lennard Timm <hi@lenn4rd.io>
2019-10-28 15:22:13 +0100

    Add Docker shell aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -5,3 +5,15 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+alias db='docker build --rm'
+alias di='docker images'
+alias drm='docker rm'
+alias drmi='docker rmi'
+alias drmid='docker rmi $(docker images -f dangling=true -q)'
+
+alias dc='docker-compose'
+alias dcex='docker-compose exec'
+alias dcps='docker-compose ps'
+alias dcr='docker-compose restart'
+alias dcrld='docker-compose up --no-start'
