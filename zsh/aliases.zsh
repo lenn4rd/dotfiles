9f0a7cc9906eff30b565aa4ea16c49a771141377
Lennard Timm <hi@lenn4rd.io>
2019-10-28 15:33:26 +0100

    Combine shell aliases into one file

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,10 +1,28 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 
-alias ll='ls -lh'
-alias la='ls -lah'
-
 alias rld='. ~/.zshrc'
 
-alias dotfiles='mate ~/.dotfiles'
-alias zshrc='mate ~/.zshrc'
+if $(gls &> /dev/null)
+then
+  alias la='gls -A --color'
+  alias ll="gls -l --color"
+  alias ls="gls -F --color"
+fi
+
+alias la='ls -lah'
+alias ll='ls -lh'
+
+# Docker
+alias db='docker build --rm'
+alias di='docker images'
+alias drm='docker rm'
+alias drmi='docker rmi'
+alias drmid='docker rmi $(docker images -f dangling=true -q)'
+
+# Docker Compose
+alias dc='docker-compose'
+alias dcex='docker-compose exec'
+alias dcps='docker-compose ps'
+alias dcr='docker-compose restart'
+alias dcrld='docker-compose up --no-start'
