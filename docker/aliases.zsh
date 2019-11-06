8cfb34290d6eed1992517f59ad57b143c96f9dac
Lennard Timm <hi@lenn4rd.io>
2019-11-06 12:13:48 +0100

    Move aliases into their subdirectory

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,2 +1,13 @@
-alias d='docker $*'
-alias d-c='docker-compose $*'
+# Docker
+alias db='docker build --rm'
+alias di='docker images'
+alias drm='docker rm'
+alias drmi='docker rmi'
+alias drmid='docker rmi $(docker images -f dangling=true -q)'
+
+# Docker Compose
+alias dc='docker-compose'
+alias dcex='docker-compose exec'
+alias dcps='docker-compose ps'
+alias dcr='docker-compose restart'
+alias dcrld='docker-compose up --no-start'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,17 +12,3 @@ fi
 
 alias la='ls -lah'
 alias ll='ls -lh'
-
-# Docker
-alias db='docker build --rm'
-alias di='docker images'
-alias drm='docker rm'
-alias drmi='docker rmi'
-alias drmid='docker rmi $(docker images -f dangling=true -q)'
-
-# Docker Compose
-alias dc='docker-compose'
-alias dcex='docker-compose exec'
-alias dcps='docker-compose ps'
-alias dcr='docker-compose restart'
-alias dcrld='docker-compose up --no-start'
