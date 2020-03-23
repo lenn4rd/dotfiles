b3a9319b35782debf08a9dc6b953c1e03b53cfbf
Lennard Timm <hi@lenn4rd.io>
2020-03-23 16:21:46 +0100

    Update Ruby environment

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -1,3 +1,6 @@
 # Use multiple threads when installing gems
 export BUNDLE_JOBS=4
 export MAKEFLAGS=-j4
+
+# Use Homebrew's OpenSSL
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
