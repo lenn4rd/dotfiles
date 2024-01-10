115556cf9566328dd649915cf463f7430f4cf252
Lennard Timm <hi@lenn4rd.io>
2024-01-10 11:55:43 +0100

    Configure theme

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -3,5 +3,6 @@ for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then
   fpath=($topic_folder $fpath)
 fi;
 
-fpath=($HOME/.dotfiles/functions $fpath)
+fpath+=($HOME/.dotfiles/functions $fpath)
+fpath+=("$(brew --prefix)/share/zsh/site-functions")
 autoload -U $HOME/.dotfiles/functions/*(:t)
