ef2f6ccdf42fff6c561ae8d1e21ef7cf06afe94d
Lennard Timm <hi@lenn4rd.io>
2024-06-18 20:25:15 +0200

    Update Ruby ENV

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -4,3 +4,6 @@ export MAKEFLAGS=-j$(nproc)
 
 # Use Homebrew's OpenSSL
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
+BUNDLE_EXEC_GEMFILE_CURRENT_DIR_ONLY=yes
+BUNDLE_EXEC_COMMANDS='annotate rails rake rspec spring'
