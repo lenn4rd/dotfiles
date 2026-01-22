1838b05a6c4bf982e449799ca6f87ab3878511b1
Lennard Timm <hi@lenn4rd.io>
2026-01-22 13:35:27 +0100

    Parse Kubernetes config files correctly

--- kubernetes/env.zsh
+++ kubernetes/env.zsh
@@ -1,8 +1,11 @@
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+KUBECONFIG=$HOME/.kube/config
 
 # Set up kubectx and kubens
-for file in $(find $HOME/.kube -type f -d 1 \! -name kubectx); do
+for file in $(find $HOME/.kube -type f -d 1 \! -name config \! -name kubectx | sort); do
   KUBECONFIG=${KUBECONFIG:+$KUBECONFIG:}$file
 done
 
 export KUBECONFIG
+
+export MINIKUBE_IN_STYLE=false
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
