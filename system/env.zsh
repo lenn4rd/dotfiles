d2d49f4ede4a5a19bf266b1dff4714506d8901c0
Lennard Timm <hi@lenn4rd.io>
2026-01-22 13:34:34 +0100

    Merge system topic into zsh

--- system/env.zsh
+++
@@ -1,5 +0,0 @@
-# Don't unset this project default
-export ZSH=$HOME/.dotfiles
-
-export EDITOR=nano
-export PATH="$HOME/.dotfiles/bin:$PATH"
---
+++ zsh/env.zsh
@@ -0,0 +1,6 @@
+# Don't unset this project default
+export ANTIDOTE_HOME=~/.cache/antidote
+export ZSH=$HOME/.dotfiles
+
+export EDITOR=nano
+export PATH="$HOME/.dotfiles/bin:$HOME/.local/bin:$PATH"
